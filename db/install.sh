4da390709359b3494c4a4b2b29cf3330cd0a00b8
Heron Yang <heron.yang.tw@gmail.com>
2017-02-11 20:32:31 -0500

    tiny update

---
+++ db/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+# Ubuntu 16.04
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+# nodejs
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+npm install
