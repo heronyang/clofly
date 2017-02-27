58694eb88c7158ef2850beec9e4c6a3de0ab4f5f
Heron Yang <heron.yang.tw@gmail.com>
2017-02-26 22:24:17 -0500

    install user function dependencies automatically in build

---
+++ server/node-template/install-user-dependencies.js
@@ -0,0 +1,27 @@
+console.log('[Start] Install user function depedencies');
+
+var fs = require('fs');
+
+var f = fs.readFileSync('user-function.js', 'utf8');
+var re = /require\(.*\)/gi;
+var matches = f.match(re);
+
+var deps = '';
+matches.forEach(function(match) {
+    var dep = match.substring(9,match.length-2)
+    console.log(dep);
+    deps += dep + ' ';
+});
+
+var command = 'npm install --save ' + deps;
+var exec = require('child_process').exec;
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+console.log('[Done] Install user function depedencies');
